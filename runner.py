cfd1f3d1d855067e0bf9f91e485eb3cbfe2a49a2
julien9969 <julien.roux404@gmail.com>
2024-10-03 18:15:26 -0400

    runner fix

--- runner.py
+++ runner.py
@@ -11,11 +11,11 @@ DIR = "./Divercite"
 ENV = "INF8175"
 SHELL = "zsh"
 
-agent1_name = "greedy_player_divercite.py" #"2000.py"
-agent2_name = "greedy_player_divercite.py" #findus200v2.py"
+# agent1_name = "greedy_player_divercite.py" #"2000.py"
+# agent2_name = "greedy_player_divercite.py" #findus200v2.py"
 
-# agent1_name = "findus200v2.py"
-# agent2_name = "2000.py"
+agent1_name = "findus200v2.py"
+agent2_name = "2000.py"
 
 class GameResult:
     def __init__(self, i, agent1, score1, time1, agent2, score2, time2, winner):
@@ -45,7 +45,7 @@ def play_game(agent1: str, agent2: str, i: int, port):
     if sys.platform == "win32":
         command = f"cmd /c \"cd {DIR} && conda activate {ENV} && python main_divercite.py -t local {agent1} {agent2} -g -p {port}\""
     else:
-        command = f"{SHELL} -c \"cd {DIR} && source ~/.{SHELL.lower()}rc && conda activate {ENV} && python main_divercite.py -t local {agent1} {agent2} -g -p {port}\""
+        command = f"{SHELL} -c \"cd {DIR} && source ~/.{SHELL}rc && conda activate {ENV} && python main_divercite.py -t local {agent1} {agent2} -g -p {port}\""
     
     print(f"Start Game {i} - {agent1} vs {agent2} on port {port}")
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, encoding="cp437")
